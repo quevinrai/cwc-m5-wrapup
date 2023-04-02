a0eaa6b9f2c4790977f739f65058c24a07b6fcb3
Quevin Bambasi <quevinbambasi@pm.me>
2023-04-01 22:33:18 -0400

    Initial Commit

---
+++ M5 Wrapup Challenge/M5_Wrapup_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M5_Wrapup_ChallengeApp.swift
+//  M5 Wrapup Challenge
+//
+//  Created by Quevin Bambasi on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct M5_Wrapup_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
